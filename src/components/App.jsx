4d9fc2612efe228a028514b18defd7a4eb256b89
Yunus Irmak <yunusirmak75@hotmail.com>
2021-09-29 23:55:04 +0300

    basic form

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Portfolio App</h1>
+      <form action="">
+        <input type="text" placeholder="Username" id="username" />
+        <input type="password" placeholder="Password" id="password" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
