b7115918792f2fffdb667df929179585401db415
Yunus Irmak <yunusirmak75@hotmail.com>
2021-09-30 21:46:39 +0300

    hooks folder and basic users array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 function App() {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const [users, setUsers] = useState([]);
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setUser((prevUser) => {
+      return {
+        ...prevUser,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    setUsers([...users, user]);
+    event.preventDefault();
+  }
+
+  function showUsers(userInfo) {
+    return (
+      <div>
+        <ul>
+          <li>{userInfo.username}</li>
+          <li>{userInfo.password}</li>
+        </ul>
+      </div>
+    );
+  }
   return (
     <div>
       <h1>Portfolio App</h1>
       <form action="">
-        <input type="text" placeholder="Username" id="username" />
-        <input type="password" placeholder="Password" id="password" />
-        <button>Submit</button>
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="Username"
+          name="username"
+          value={user.username}
+        />
+        <input
+          type="password"
+          onChange={handleChange}
+          placeholder="Password"
+          name="password"
+          value={user.password}
+        />
+        <button onClick={handleSubmit}>Submit</button>
       </form>
+      <br />
+      <div>{users.map(showUsers)}</div>
     </div>
   );
 }
