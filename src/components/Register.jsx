1a4ec2052e850dff7827b8c1be1050a0c9bfd0f0
Yunus Irmak <yunusirmak75@hotmail.com>
2021-10-02 19:40:43 +0300

    successfully logged in

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,59 @@
 import React, { useState } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 function Register() {
   const [isSignUp, setSignUp] = useState(false);
+
+  const [credential, setCredential] = useState({
+    cMail: "",
+    cPassword: "",
+  });
+
+  const [user, setUser] = useState({
+    name: "",
+    mail: "",
+    password: "",
+  });
+
+  const [users, setUsers] = useLocalStorage("users", []);
+
+  function signUpChange(event) {
+    const { name, value } = event.target;
+
+    setUser((prevUser) => {
+      return {
+        ...prevUser,
+        [name]: value,
+        balance: 10000,
+      };
+    });
+  }
+  function signInChange(event) {
+    const { name, value } = event.target;
+
+    setCredential((prevCredential) => {
+      return {
+        ...prevCredential,
+        [name]: value,
+      };
+    });
+  }
+  function handleSignUp(event) {
+    setUsers([user]);
+    setSignUp(false);
+    event.preventDefault();
+  }
+  function handleSignIn(event) {
+    if (
+      users[0].mail === credential.cMail &&
+      users[0].password === credential.cPassword
+    ) {
+      console.log("Credentials are true!");
+    } else {
+      console.log("Credentials are false!");
+    }
+    event.preventDefault();
+  }
   return (
     <div id="register-body">
       <div
@@ -12,22 +64,59 @@ function Register() {
           <form id="register-form" action="#">
             <h1 id="register-h1">Create Account</h1>
             <span id="register-span">use your email for registration</span>
-            <input id="register-input" type="text" placeholder="Name" />
-            <input id="register-input" type="email" placeholder="Email" />
-            <input id="register-input" type="password" placeholder="Password" />
-            <button class="register-button">Sign Up</button>
+            <input
+              id="register-input"
+              onChange={signUpChange}
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={user.name}
+            />
+            <input
+              id="register-input"
+              onChange={signUpChange}
+              type="email"
+              name="mail"
+              placeholder="Email"
+              value={user.mail}
+            />
+            <input
+              id="register-input"
+              onChange={signUpChange}
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={user.password}
+            />
+            <button class="register-button" onClick={handleSignUp}>
+              Sign Up
+            </button>
           </form>
         </div>
         <div class="form-container sign-in-container">
           <form id="register-form" action="#">
             <h1 id="register-h1">Sign in</h1>
             <span id="register-span">with your account</span>
-            <input id="register-input" type="email" placeholder="Email" />
-            <input id="register-input" type="password" placeholder="Password" />
+            <input
+              id="register-input"
+              onChange={signInChange}
+              type="email"
+              name="cMail"
+              placeholder="Email"
+            />
+            <input
+              id="register-input"
+              onChange={signInChange}
+              type="password"
+              name="cPassword"
+              placeholder="Password"
+            />
             <a href="#" id="register-a">
               Forgot your password?
             </a>
-            <button class="register-button">Sign In</button>
+            <button onClick={handleSignIn} class="register-button">
+              Sign In
+            </button>
           </form>
         </div>
         <div class="overlay-container">
